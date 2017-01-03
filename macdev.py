9db00cf8de8ca1b060f2f5349f3b605d1a38d787
MacRae Linton <macrae.linton@gmail.com>
2017-01-02 17:22:14 -0800

    Vim is configed and brew is installed

--- macdev.py
+++ macdev.py
@@ -8,18 +8,42 @@ import subprocess
 MACDEV_PATH = os.path.dirname(os.path.realpath(__file__))
 DOTFILES_DIR = "dotfiles"
 
+def silent_call(cmd):
+    with open(os.devnull, 'w') as devnull:
+        retcode = subprocess.call(cmd, stdout=devnull, stderr=subprocess.STDOUT)
+        return retcode
+
+def install_xcode_tools():
+    xcode_cmd = ["xcode-select", "--install"]
+    retcode = silent_call(xcode_cmd)    
+    if retcode == 0:
+        print("rerun after you've installed the xcode tools")
+        exit(0)
+    else:
+        print("xcode tools like git are installed")
+
+def install_brew():
+    if not os.path.isfile("/usr/local/bin/brew"):
+        print("installing brew")
+        brew_url = "https://raw.githubusercontent.com/Homebrew/install/master/install"
+        brew_script = urllib2.urlopen(brew_url)
+        brew_cmd = ["/usr/bin/ruby", "-e", brew_script.read()]
+        subprocess.call(brew_cmd)
+
 def remake_dir(path):
     full_path = os.path.expanduser(path)
     if not os.path.isdir(full_path):
         os.makedirs(full_path)
 
 def mk_dirs():
+    print("creating some standard directories")
     # create a few standard directories
     basic_dirs = ["~/bin", "~/code"]
     for bdir in basic_dirs:
         remake_dir(bdir)
 
 def copy_dotfiles():
+    print("copying all dotfiles")
     dotfile_dir = os.path.join(MACDEV_PATH, DOTFILES_DIR)
     dotfiles = os.listdir(dotfile_dir)
     for dotfile in dotfiles:
@@ -38,6 +62,7 @@ def install_pathogen():
             pathogen_file.write(pathogen.read())
 
 def vim_plugins():
+    print("configuring vim")
     vim_dirs = ["~/.vim/autoload", "~/.vim/bundle"]
     for vdir in vim_dirs:
         remake_dir(vdir)
@@ -47,7 +72,8 @@ def vim_plugins():
             "git://github.com/tpope/vim-sensible.git",
             "git://github.com/tpope/vim-surround.git",
             "git://github.com/tpope/vim-commentary.git",
-            "git://github.com/ctrlpvim/ctrlp.vim.git"
+            "git://github.com/ctrlpvim/ctrlp.vim.git",
+            "git://github.com/altercation/vim-colors-solarized.git"
             ]
 
     dest_extractor = r'/([^/]+).git'
@@ -56,12 +82,13 @@ def vim_plugins():
         dest = os.path.expanduser("~/.vim/bundle/" + dest_name)
         if not os.path.isdir(dest):
             git_cmd = ["git", "clone", plugin_url, dest]
-            with open(os.devnull, 'w') as devnull:
-                subprocess.check_call(git_cmd, stdout=devnull, stderr=subprocess.STDOUT)
+            silent_call(git_cmd)
     
 
 if __name__ == "__main__":
     print("we on it")
+    install_xcode_tools()
+    install_brew()
     mk_dirs()
     copy_dotfiles()
     vim_plugins()
